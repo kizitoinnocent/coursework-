80c24b5e90f7bbb2a0ba7039f6ad2974c6737c1a
kizitoinnocent <117288618+kizitoinnocent@users.noreply.github.com>
2022-11-10 10:32:11 +0300

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,61 @@
+package GFG;
+/*<APPLET code = "GFG.class" width=500 height =500>
+ </APPLET>*/
+
+//Java Code to implement moving text using applet and thread.
+
+import java.awt.*;
+import java.applet;
+
+public class GFG extends Applet implements Runnable{
+	private String display;
+	private int x, y, flag;
+	Thread t;
+	
+	//initializing
+	//called when the applet is
+	//started.
+
+	public void init() {
+		display ="GeeksforGeeks";
+		x = 100;
+		y = 100;
+		flag = 1;
+		
+		// creating thread
+		t =  new Thread(this, " My Thread");
+		
+		// start thread
+		t.start();
+	}
+   // update the x co-ordinate
+	public void update() {
+		
+		x = x + 10 * flag;
+		if (x < 300)flag = -1;
+		if (x < 100)flag = -1;	
+	}
+	//run
+	public void run() {
+		while (true) {
+			//Repaiting the screen
+			//calls the paint fuction repaint();
+			
+			update();
+			try {
+				//creating a pause of 1 second
+				//so that the movement is recognizable
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException ie) {
+				System.out.println(ie);	
+			}
+		}		
+	}
+	//drawString
+	public void paint(Graphics g)
+	{
+		g.drawString(display,x,y);
+	}
+}
+
